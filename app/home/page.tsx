68d376d115c495525c4f906a45b0db900c30c9f6
MuhammadFahrial <rialjhe111@gmail.com>
2025-05-20 11:06:04 +0800

    update: informasi acara

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -27,12 +27,12 @@ export default function Home() {
   };
 
   return (
-    <div className="overflow-hidden">
+    <div className="box-border overflow-hidden">
       {/* Ucapan */}
       <section
         ref={heroRef}
         id="home"
-        className="flex flex-col h-screen justify-center items-center bg-amber-100 text-black bg-cover bg-center"
+        className="flex flex-col h-screen justify-center items-center bg-amber-100 text-black bg-cover bg-center "
         // style={{ backgroundImage: "url('/bg-hero.jpg')" }}
       >
         <TimelineContent
@@ -105,7 +105,39 @@ export default function Home() {
       </section>
 
       {/* Informasi Acara */}
-      <section id="informasi"></section>
+      <section id="informasi">
+        <div className="flex flex-col items-center justify-center py-12 text-black bg-amber-200">
+          <h1 className={`mb-8 ${playball.className} text-4xl`}>
+            Informasi Acara
+          </h1>
+          <div className="flex flex-col items-center justify-center md:flex-row gap-12">
+            <div className="flex flex-row justify-center items-center w-100 md:w-1/2">
+              <Image
+                src={"/map.png"}
+                alt="map"
+                width={60}
+                height={10}
+                className="w-10 h-10 mr-4 hidden md:block"
+              />
+              <h1
+                className={`text-center ${crimsonText.className} text-xl md:text-left`}
+              >
+                Jalan Salla Desa No.48, Belopa, Kec. Belopa, Kabupaten Luwu,
+                Sulawesi Selatan 91994
+              </h1>
+            </div>
+            <div>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.8248628858596!2d120.37382847405696!3d-3.3928955965817202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d96bb44222ff387%3A0xde22ec82bd984dcc!2sDUA%20PUTRA%20KOMPUTER%20(Servise%20Dan%20Sablon%20Polyflex)!5e0!3m2!1sen!2sid!4v1747708143016!5m2!1sen!2sid"
+                // width="400"
+                // height="350"
+                loading="lazy"
+                className="w-84 h-84 rounded-2xl"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* Gallery */}
       <section id="gallery" className={``}></section>
